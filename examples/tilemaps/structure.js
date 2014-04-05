3016c959011ed6216fd8e29fa9f4cbf8d526fac6
Wesley Reardan <reardan@gmail.com>
2014-04-04 23:43:09 -0500

    Added structure and unit classes to Entity file.  Removed structure.js file

--- examples/tilemaps/structure.js
+++
@@ -1,7 +0,0 @@
-function Structure(game, x, y, player_id, sprite_name) {
-	Entity.call(this, game, x, y, player_id, sprite_name) //Call the Parent Constructor
-}
-
-Structure.prototype = new Entity()
-Structure.prototype.constructor = Structure
-
